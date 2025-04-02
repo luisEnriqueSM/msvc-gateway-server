feb6c5e9e94822a43fa07e302b4bdd16f710f17a
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-04-02 12:48:02 -0600

    Get User roles from token

--- src/main/java/com/luis/springcloud/app/gateway/security/SecurityConfig.java
+++ src/main/java/com/luis/springcloud/app/gateway/security/SecurityConfig.java
@@ -2,10 +2,22 @@ package com.luis.springcloud.app.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
+
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
@@ -21,8 +33,18 @@ public class SecurityConfig {
         }).cors(csrf -> csrf.disable())
             .oauth2Login(withDefaults())
             .oauth2Client(withDefaults())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-            .build();
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                jwt -> jwt.jwtAuthenticationConverter(new Converter<Jwt, Mono<AbstractAuthenticationToken>>() {
+                    @Override
+                    public Mono<AbstractAuthenticationToken> convert(Jwt source) {
+                        Collection<String> roles = source.getClaimAsStringList("roles");
+                        Collection<GrantedAuthority> authorities = roles.stream()
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toList());
+                        return Mono.just(new JwtAuthenticationToken(source, authorities));
+                    }
+                })
+            )).build();
     }
 
 }
